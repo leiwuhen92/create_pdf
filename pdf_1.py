8a91c4ad1086b74e3a69450748f8da150e5171a8
leiwuhen92 <1162886013@qq.com>
2019-09-22 11:53:46 +0800

    抓取网页存为pdf

--- pdf_1.py
+++ pdf_1.py
@@ -3,11 +3,22 @@
 import pdfkit
 
 url = "https://10.21.144.110:8443/#/dashboard"
+options = {
+                         'page-size': 'Letter',
+                         'margin-top': '0.75in',
+                         'margin-right': '0.75in',
+                         'margin-bottom': '0.75in',
+                         'margin-left': '0.75in',
+                         'encoding': "UTF-8",
+                         'no-outline': None
+                    }
 
-confg =pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+path_wk = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+confg = pdfkit.configuration(wkhtmltopdf=path_wk)
+
 
 #pdfkit.from_url(url,'url.pdf',configuration=confg)
-pdfkit.from_file('百度.html', '百度.pdf',configuration=confg)
+pdfkit.from_file('百度.html', '百度.pdf',configuration=confg,options=options)
 
 
 html='''
